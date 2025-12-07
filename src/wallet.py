5e96721de0425419ad4ffdd8c00eb90341a07b27
recurzes <donlancelotknight123@gmail.com>
2025-12-07 18:43:41 +0800

    Rename `Deposit` to `Wallet` and extend functionality with deposit, withdrawal, deduction, and win processing methods.

--- src/wallet.py
+++ src/wallet.py
@@ -1,7 +1,10 @@
 import models
 import db
-class Deposit:
-    def process_deposit(self, user: models.User, amount: float):
+class Wallet:
+    def __init__(self, user: models.User):
+        self.user = user
+
+    def process_deposit(self, amount: float):
         if amount <= 0:
             print("❌ Deposit amount must be positive.")
             return False
@@ -15,10 +18,10 @@ class Deposit:
                     SET balance = balance + ?
                     WHERE username = ?
                     """
-            cursor.execute(query, (amount, user.get_username()))
+            cursor.execute(query, (amount, self.user.get_username()))
             connection.commit()
 
-            user.update_balance(amount)
+            self.user.update_balance(amount)
             print(f"\n💰 Deposit of ${amount:.2f} successful!")
             print("")
             return True
@@ -32,3 +35,83 @@ class Deposit:
                 cursor.close()
             if connection:
                 connection.close()
+
+    def process_withdrawal(self, amount: float):
+        if amount <= 0:
+            print("Deposit amount must be positive")
+
+        try:
+            connection = db.get_connections()
+            cursor = connection.cursor()
+
+            query = """
+                    UPDATE users
+                    SET balance = balance - ?
+                    WHERE username = ?
+                    """
+
+            cursor.execute(query, (amount, self.user.get_username()))
+            connection.commit()
+
+            self.user.update_balance(amount)
+            print(f"Withdrawal of ${amount:.2f} to GCash number {self.user.get_user_info(6)} successful!")
+            print("")
+            return True
+
+        except Exception as e:
+            print("Withdrawal failed")
+            return False
+
+        finally:
+            if cursor:
+                cursor.close()
+            if connection:
+                connection.close()
+
+    def process_deduction(self, amount: float):
+        if amount <= 0:
+            print("Deposit amount must be positive")
+
+        try:
+            connection = db.get_connections()
+            cursor = connection.cursor()
+
+        except Exception as e:
+            print("Deduction failed")
+            return False
+
+        finally:
+            if cursor:
+                cursor.close()
+            if connection:
+                connection.close()
+
+    def process_win(self, amount: float):
+        if amount <= 0:
+            print("Winning amount must be positive")
+
+        try:
+            connection = db.get_connections()
+            cursor = connection.cursor()
+
+            query = """
+                    UPDATE users
+                    SET balance = balance + ?
+                    WHERE username = ?
+                    """
+
+            cursor.execute(query, (amount, self.user.get_username()))
+            connection.commit()
+
+            self.user.update_balance(amount)
+            print(f"You won a total of ${amount:.2f} and has been added to account!")
+            print("")
+            return True
+
+        except Exception as e:
+            print("Adding winnings failed")
+            return False
+
+
+
+
